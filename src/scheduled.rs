21f05617b55477e498527dcd5e397ca969877482
sean <seanwidmier@gmail.com>
2024-10-19 10:00:44 -0700

    fix[scheduled]: move from trylock to just lock for scheduled threads

--- src/scheduled.rs
+++ src/scheduled.rs
@@ -3,8 +3,6 @@ use crate::data::db::BotDatabase;
 
 use log::{info, warn};
 use poise::serenity_prelude as serenity;
-use rand::rngs::{OsRng, StdRng};
-use rand::{Rng, SeedableRng};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::time::{sleep, Duration};
@@ -26,15 +24,36 @@ pub async fn spawn_scheduled_tasks(
 
 async fn daily_bestof_task(ctx: serenity::Context, bestof: Arc<Mutex<BestOf>>) {
     loop {
-        sleep_random_time().await;
-
         if let Err(why) = post_daily_bestof(&ctx, &bestof).await {
             warn!("Failed to post daily bestof: {:?}", why);
         }
 
-        // TODO: schedule this
-        let sleep_duration = std::time::Duration::from_secs(86400);
+        let sleep_duration = Duration::from_secs(86400);
         info!("Next posting after {:?}", sleep_duration);
+        sleep(sleep_duration).await;
+    }
+}
+
+async fn reaction_counting_task(ctx: serenity::Context, bestof: Arc<Mutex<BestOf>>) {
+    loop {
+        if let Err(why) = update_bestof_runtime_db(&ctx, &bestof).await {
+            warn!("Failed to update bestof runtime data: {:?}", why);
+        }
+
+        let sleep_duration = Duration::from_secs(600);
+        info!("Next counting reactions after {:?}", sleep_duration);
+        sleep(sleep_duration).await;
+    }
+}
+
+async fn persistent_database_update_task(db: Arc<Mutex<BotDatabase>>, bestof: Arc<Mutex<BestOf>>) {
+    loop {
+        if let Err(why) = update_bestof_persisted_db(&db, &bestof).await {
+            warn!("Failed to persist bestof data: {:?}", why);
+        }
+
+        let sleep_duration = Duration::from_secs(3610);
+        info!("Next reaction counting after {:?}", sleep_duration);
         sleep(sleep_duration).await;
     }
 }
@@ -44,81 +63,36 @@ async fn post_daily_bestof(
     bestof: &Arc<Mutex<BestOf>>,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let update_channel = serenity::ChannelId::new(563105728341082148); // DM for now
-    let mut bestof_unlocked =
-        acquire_lock(bestof).map_err(|e| format!("bestof lock error: {:?}", e))?;
+    let mut bestof_unlocked = bestof.lock().await;
 
-    bestof_unlocked.post_random(ctx, update_channel).await
-}
-
-async fn reaction_counting_task(ctx: serenity::Context, bestof: Arc<Mutex<BestOf>>) {
-    loop {
-        sleep_random_time().await;
-
-        if let Err(why) = update_bestof_runtime_db(&ctx, &bestof).await {
-            warn!("Failed to update bestof runtime data: {:?}", why);
-        }
-
-        let sleep_duration = std::time::Duration::from_secs(600);
-        info!("Next counting reactions after {:?}", sleep_duration);
-        sleep(sleep_duration).await;
-    }
+    bestof_unlocked
+        .post_random(
+            ctx,
+            update_channel,
+            Some(String::from("*Here's your daily bestof:*")),
+        )
+        .await
 }
 
 async fn update_bestof_runtime_db(
     ctx: &serenity::Context,
     bestof: &Arc<Mutex<BestOf>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut bestof_unlocked =
-        acquire_lock(bestof).map_err(|e| format!("bestof lock error: {:?}", e))?;
+    let mut bestof_unlocked = bestof.lock().await;
 
     bestof_unlocked.update(ctx).await?;
     Ok(())
-}
-
-async fn persistent_database_update_task(db: Arc<Mutex<BotDatabase>>, bestof: Arc<Mutex<BestOf>>) {
-    loop {
-        sleep_random_time().await;
-
-        if let Err(why) = update_bestof_persisted_db(&db, &bestof).await {
-            warn!("Failed to persist bestof data: {:?}", why);
-        }
-
-        // Once an hour, offset from runtime db update by 10 seconds.
-        let sleep_duration = std::time::Duration::from_secs(3610);
-        info!("Next reaction counting after {:?}", sleep_duration);
-        sleep(sleep_duration).await;
-    }
-}
-
-/// Random jitter for the startup of tasks so they're not all holding
-/// the database lock at the same time.
-pub async fn sleep_random_time() {
-    // Generate a random number between 1 and 30
-    let mut rng = StdRng::from_rng(OsRng).unwrap();
-    let sleep_secs = rng.gen_range(1..=30);
-
-    // Convert to a Duration
-    let sleep_duration = Duration::from_secs(sleep_secs);
-
-    // Sleep for the random amount of time
-    sleep(sleep_duration).await;
 }
 
 async fn update_bestof_persisted_db(
     db: &Arc<Mutex<BotDatabase>>,
     bestof: &Arc<Mutex<BestOf>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let mut bestof_unlocked =
-        acquire_lock(bestof).map_err(|e| format!("bestof lock error: {:?}", e))?;
-    let mut db_unlocked = acquire_lock(db).map_err(|e| format!("db lock error: {:?}", e))?;
+    let mut bestof_unlocked = bestof.lock().await;
+    let mut db_unlocked = db.lock().await;
 
     bestof_unlocked
         .update_persisted_db(&mut db_unlocked)
         .await?;
     Ok(())
-}
-
-fn acquire_lock<T>(lock: &Arc<Mutex<T>>) -> Result<tokio::sync::MutexGuard<'_, T>, String> {
-    lock.try_lock()
-        .map_err(|_| "Couldn't acquire lock".to_string())
 }
